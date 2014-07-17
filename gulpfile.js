37f145c40ec2c973134bf042d437db1b0578b7e9
Ahmet Katrancı <ahmet@katranci.co.uk>
2014-07-17 19:43:10 +0100

    gulp-jshint

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,25 @@
-var gulp = require('gulp'),
-    concat = require('gulp-concat'),
-    uglify = require('gulp-uglify'),
-    rename = require('gulp-rename');
+var gulp    = require('gulp'),
+    concat  = require('gulp-concat'),
+    uglify  = require('gulp-uglify'),
+    jshint  = require('gulp-jshint'),
+    stylish = require('jshint-stylish'),
+    rename  = require('gulp-rename');
 
 
 gulp.task('default', function() {
   gulp.src(['./src/rAF.js', './src/PositionSticky.js'])
+      .pipe(jshint())
+      .pipe(jshint.reporter(stylish))
       .pipe(concat('PositionSticky.js'))
       .pipe(gulp.dest('dist'))
       .pipe(uglify())
       .pipe(rename('PositionSticky.min.js'))
       .pipe(gulp.dest('dist'));
+});
+
+
+gulp.task('lint', function() {
+  gulp.src(['./src/rAF.js', './src/PositionSticky.js'])
+      .pipe(jshint())
+      .pipe(jshint.reporter(stylish));
 });
