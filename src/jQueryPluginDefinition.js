b1888c949e39919f91bba8b9819e03a1dbe3040b
Ahmet Katrancı <ahmet@katranci.co.uk>
2014-07-20 05:26:59 +0100

    jQuery plugin definition

---
+++ src/jQueryPluginDefinition.js
@@ -0,0 +1,16 @@
+$.fn.positionSticky = function(options) {
+
+  return this.each(function() {
+    var $this = $(this);
+    var data  = $this.data('positionSticky');
+
+    if (!data) {
+      $this.data('positionSticky', (data = PositionSticky.create(this, options)));
+    }
+
+    if (typeof options === 'string') {
+      data[options]();
+    }
+  });
+
+};
